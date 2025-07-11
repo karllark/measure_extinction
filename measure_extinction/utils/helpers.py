fa1ad56b2ee1c91d256d15366bae019ac58f2e2b
Karl Gordon <kgordon@stsci.edu>
2025-07-11 10:01:44 -0400

    removing old multiple spectra plotting

--- measure_extinction/utils/helpers.py
+++ measure_extinction/utils/helpers.py
@@ -1,31 +1,7 @@
 import os
 import importlib.resources as importlib_resources
 
-__all__ = ["get_full_starfile", "get_datapath"]
-
-
-def get_full_starfile(starname):
-    """
-    Get full path to the DAT file for the specified star
-    """
-    if "dat" not in starname:
-        fstarname = "%s.dat" % starname
-    else:
-        fstarname = starname
-
-    if not os.path.isfile(fstarname):
-        # not an installable package yet
-        # def_path = pkg_resources.resource_filename(
-        #    'extstar_data', '')
-        def_path = "/Users/pyanchulova/Documents/extstar_data/"
-        if not os.path.isfile("{}/DAT_files/{}".format(def_path, fstarname)):
-            print("{} file not found".format(fstarname))
-        else:
-            fstarname = "DAT_files/{}".format(fstarname)
-    else:
-        def_path = "./"
-
-    return fstarname, def_path
+__all__ = ["get_datapath"]
 
 
 def get_datapath():
