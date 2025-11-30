3e2b28a77b36c42bbaabcb3285c5ce7b7c83ef8a
Karl Gordon <kgordon@stsci.edu>
2025-11-30 13:57:26 -0500

    adding correction to HST flux scale

--- measure_extinction/utils/merge_iue_spec.py
+++ measure_extinction/utils/merge_iue_spec.py
@@ -1,12 +1,15 @@
 import argparse
 import glob
+import warnings
 
 import numpy as np
 import astropy.units as u
 from astropy.table import QTable
+from astropy.io import fits
 import matplotlib.pyplot as plt
 
 from measure_extinction.merge_obsspec import merge_iue_obsspec
+from measure_extinction.utils.helpers import get_datapath
 
 
 if __name__ == "__main__":
@@ -29,18 +32,35 @@ if __name__ == "__main__":
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
 
-    # incomplete - code to read in the files needed
-    # use_small capability needed
-    # recalibration by Massa & Fitzpatrick IDL code needs to be recoded in python
+    # read in the correction factors to put everything on the HST flux calibration scale
+    # from Bohlin & Bianchi 2018, AJ, 155, 162
+    dpath = get_datapath()
+    lcor = QTable.read(f"{dpath}/hlsp_iue-fluxcal_iue_lwp-lwr_cal_low-disp_v1_corr.txt",
+                       format="ascii")
+    lcor.rename_column("col1", "wavelength")
+    lcor.rename_column("col2", "LWP")
+    lcor.rename_column("col3", "LWR")
+
+    scor = QTable.read(f"{dpath}/hlsp_iue-fluxcal_iue_swp_cal_low-disp_v1_corr.txt",
+                       format="ascii")
+    scor.rename_column("col1", "wavelength")
+    scor.rename_column("col2", "SWP")
 
     stable = []
     sfilename = f"{args.iuedir}/*.mxlo"
     sfiles = glob.glob(sfilename)
     for cfile in sfiles:
         print(cfile)
+        warnings.filterwarnings('ignore', category=u.UnitsWarning, append=True)
         tt1 = QTable.read(cfile, format="fits")
-        t1 = QTable()
+        tt1_header = fits.getheader(cfile)
+        camera = tt1_header["CAMERA"]
+        aperture = tt1_header["APERTURE"]
+        if aperture == "SMALL":
+            print("Small aperture not supported yet")
+            exit()
 
+        t1 = QTable()
         minwave = tt1["WAVELENGTH"][0].value
         dwave = tt1["DELTAW"][0].value
         nwave = tt1["NPOINTS"][0]
@@ -52,6 +72,17 @@ if __name__ == "__main__":
         nvals = t1["FLUX"] == 0.0
         if np.sum(nvals):
             t1["NPTS"][nvals] = 0.0
+
+        # correct the data to the HST absolute flux calibration scale
+        if camera[0] == "L":
+            cwaves = lcor["wavelength"].value
+            cvals = lcor[camera].value
+        else:
+            cwaves = scor["wavelength"].value
+            cvals = scor[camera].value
+        corvals = np.interp(t1["WAVELENGTH"].value, cwaves, cvals)
+        t1["FLUX"] = corvals * t1["FLUX"]
+
         stable.append(t1)
 
     rb_iue = merge_iue_obsspec(stable)
